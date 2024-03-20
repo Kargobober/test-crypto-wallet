273c06367e28cda056a90b99dd81870864542dbf
Kargobober <kargobober@yandex.ru>
2024-03-20 17:22:42 +0300

    add homepage configure MUI&Next

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { CssBaseline } from "@mui/material";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'; // https://mui.com/material-ui/integrations/nextjs/
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +11,13 @@ export const metadata: Metadata = {
   description: "test task",
 };
 
+// https://mui.com/material-ui/getting-started/usage/#responsive-meta-tag
+// https://nextjs.org/docs/app/api-reference/functions/generate-viewport
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,7 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppRouterCacheProvider>
+          <CssBaseline />
+          {children}
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+import { Button } from "@mui/material";
+import Link from '@mui/material/Link';
+import NextLink from 'next/link';
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      Main
+      <section className={styles.section}>
+        <h1>Crypto Wallet</h1>
+
+        <p>С возвращением!</p>
+
+        <Button
+          variant='contained'
+          href='/wallet'
+          component={NextLink}
+          className={styles.btn}
+        >
+          Открыть кошелёк
+        </Button>
+      </section>
     </main>
   );
 }
