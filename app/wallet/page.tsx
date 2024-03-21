c82eb8333f74036701e8705e60b7bdff81578358
Kargobober <kargobober@yandex.ru>
2024-03-21 16:46:43 +0300

    add template for React dapp with global state

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -1,6 +1,13 @@
+import { MetaMaskContextProvider } from '../lib/hooks/useMetaMask';
+import styles from './page.module.css';
+
 function Page() {
   return (
-    <div>page</div>
+    <MetaMaskContextProvider>
+      <div className={styles.appContainer}>
+        1234567
+      </div>
+    </MetaMaskContextProvider>
   )
 }
 
